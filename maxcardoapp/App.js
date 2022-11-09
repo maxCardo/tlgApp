e00aa1b97b2f6eb33170a5a9a1571e5cc152334e
Shehryar <shehryar.nadeem.iqbal2@gmail.com>
2022-11-09 16:08:43 +0500

    project setup

---
+++ maxcardoapp/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
